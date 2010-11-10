68e9a6213e5b18e6f951f443b7358198f12b5587
jboilesen <jboilesen@c1dca35a-7045-b920-d8fd-8f3a07c17feb>
2010-11-10 04:44:53 +0000

    #1 - Ficou faltando subir as classes que eu criei.

---
+++ trunk/Poupador-UNICAMP-IA/src/utils/Banco.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.awt.Point;
+
+public class Banco {
+	private Point posicao = null;
+
+	public Point getPosicao(){
+		return this.posicao;
+	}
+	public void setPosicao(Point posicao){
+		this.posicao = posicao;
+	}
+}
