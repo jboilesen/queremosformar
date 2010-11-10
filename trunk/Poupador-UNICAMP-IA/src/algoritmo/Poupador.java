44098811d38e5dd3aef3da10258bac26d9d158ea
jboilesen <jboilesen@c1dca35a-7045-b920-d8fd-8f3a07c17feb>
2010-11-10 04:43:25 +0000

    #1 - Desenvolvimento da contagem de rodadas, da classe do ambiente e da classe do banco. Já é um começo.

--- trunk/Poupador-UNICAMP-IA/src/algoritmo/Poupador.java
+++ trunk/Poupador-UNICAMP-IA/src/algoritmo/Poupador.java
@@ -1,11 +1,31 @@
 package algoritmo;
 
+import utils.Ambiente;
 
 public class Poupador extends ProgramaPoupador {
-
+	private boolean contado = false;
+	private int[] visao = null;
+	public static Ambiente ambiente = new Ambiente();
 	
 	public int acao() {
-	
+		//para sabermos quantos jogadores temos
+		if (!this.contado){
+			ambiente.contaPoupador();
+			this.contado = true;
+		}
+		
+		//primeiro guardamos a visao
+		visao = sensor.getVisaoIdentificacao();
+
+		//se nao estamos na ultima rodada
+		if (ambiente.getTicsFaltantes()>1){
+			//se aproxima da moeda mais proxima
+		}else{
+			//se pegar a moeda for seguro, pega a moeda
+			
+		}
+		//registra o movimento no ambiente
+		ambiente.contaMovimento();
 		return (int) (Math.random() * 5);
 	}
 }
