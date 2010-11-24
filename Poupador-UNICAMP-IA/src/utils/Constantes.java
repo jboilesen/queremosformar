de7a42d2f7f86c7685a00400b92d015c61dda182
jboilesen <jboilesen@c1dca35a-7045-b920-d8fd-8f3a07c17feb>
2010-11-24 11:07:40 +0000

    #0 - Arquivos Faltantes

---
+++ Poupador-UNICAMP-IA/src/utils/Constantes.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.awt.Point;
+
+public class Constantes {
+
+	// Constantes de desenvolvimento
+	public static final Point posicao = new Point(2,2);
+	public static final int Distancia_Desconhecida = -1;
+	public static final int semId = -1;
+	
+	// Constantes de Objetivo
+	public static final int semObjetivo = -1;
+	public static final int despistarLadroes = 0;
+	public static final int depositarBanco = 1;
+	public static final int pegarMoedas = 2;
+	// Constantes de Visao
+	public static final int Ve_Sem_visao_para_o_local = -2;
+	public static final int Ve_Fora_do_ambiente = -1;
+	public static final int Ve_Celula_vazia = 0;
+	public static final int Ve_Parede = 1;
+	public static final int Ve_Banco = 3;
+	public static final int Ve_Moeda = 4;
+	public static final int Ve_Pastilha_do_Poder = 5;
+	public static final int Ve_Poupador0 = 100;
+	public static final int Ve_Poupador1 = 110;
+	public static final int Ve_Ladrao = 200;
+	
+	//Constantes de Movimentacao
+	public static final int Mov_Desconhecido = -1;
+	public static final int Mov_Parado = 0;
+	public static final int Mov_Baixo = 2;
+	public static final int Mov_Acima = 1;
+	public static final int Mov_Direita = 3;
+	public static final int Mov_Esquerda = 4;
+	
+	//Percepcao olfativa
+	public static final int Sente_nenhum_cheiro = 0;
+	public static final int Sente_cheiro_muito_fraco = 1;
+	public static final int Sente_cheiro_fraco = 2;
+	public static final int Sente_cheiro_medio = 3;
+	public static final int Sente_cheiro_forte = 4;
+	public static final int Sente_cheiro_muito_forte = 5;
+
+	
+}
